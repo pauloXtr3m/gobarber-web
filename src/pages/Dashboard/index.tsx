8e061e4492f02177ae4393b5410139051227811f
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2021-02-26 00:28:09 -0300

    Feat: show appointments in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { FiPower } from 'react-icons/all';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import React, { useState } from 'react';
+import { FiClock, FiPower } from 'react-icons/all';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Calendar,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -27,6 +40,86 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="http://localhost:3333/files/e1912fde402fb6fcec60-profile.jpg"
+                alt="Paulo de Oliveira"
+              />
+
+              <strong>Paulo de Oliveira</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/e1912fde402fb6fcec60-profile.jpg"
+                  alt="Paulo de Oliveira"
+                />
+
+                <strong>Paulo de Oliveira</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/e1912fde402fb6fcec60-profile.jpg"
+                  alt="Paulo de Oliveira"
+                />
+
+                <strong>Paulo de Oliveira</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/e1912fde402fb6fcec60-profile.jpg"
+                  alt="Paulo de Oliveira"
+                />
+
+                <strong>Paulo de Oliveira</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
